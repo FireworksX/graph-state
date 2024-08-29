038da9cd8aca3af405fac5c1d9a5210b1e06db0d
fireworks <fireworksxs@gmail.com>
2024-08-29 12:45:51 +0300

    Add type for CreateState. Skip get all fields of inputs. Add new checkers.

--- packages/checkers/src/index.ts
+++ packages/checkers/src/index.ts
@@ -1,11 +1,27 @@
+import type { Graph } from '@graph-state/core'
+import { isGraph, isLinkKey } from '@graph-state/core'
+
 export const isHtmlNode = (input: any) =>
   typeof Node === 'object'
     ? input instanceof Node
     : input && typeof input === 'object' && typeof input.nodeType === 'number' && typeof input.nodeName === 'string'
 
-export const isInstanceOf = (compare: any) => (input: any) => input && input instanceof compare
+export const isInstanceOf =
+  (compare: any) =>
+  (input: any): boolean =>
+    input && input instanceof compare
 
-export const isJsxNode = (input: any) =>
+export const isJsxNode = (input: any): boolean =>
   input && typeof input.props === 'object' && input.props !== null && typeof input.type !== 'undefined'
 
-export const isLink = (input: any) => input && typeof input === 'string' && input.split(':').length >= 2
+export const isLink = (input: any): boolean => input && typeof input === 'string' && input.split(':').length >= 2
+
+export const isHtmlContent = (input: unknown): boolean =>
+  typeof input === 'string' ? /<\/?[a-z][\s\S]*>/i.test(input) : false
+
+export const isGraphOfType = (type: string) => (input: unknown) => {
+  if (isLinkKey(input)) return type === (input as string).split(':').at(0)
+  if (isGraph(input)) return (input as Graph)._type === type
+
+  return false
+}
